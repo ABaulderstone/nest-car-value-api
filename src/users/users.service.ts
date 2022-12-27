b00007aceef16b3462976f3193e24c1bb77f21e7
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-12-27 23:41:56 +1030

    adds user creation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  create(data: CreateUserDto) {
+    const user = this.userRepository.create({ ...data });
+    return this.userRepository.save(user);
+  }
+}
